7e1de9ee61d5868b08f9b6dca1db18984b6c05bf
John Henry Mejia <jmejia@vtpy10gh1j.office.atlassian.com>
2023-06-06 16:20:12 -0500

    add static website generator

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Sample data
+workout_groups = {
+    "Short": [
+        {
+            "title": "2 x 2000m rate increase",
+            "description": "Row two 2000 meter pieces...",
+            "heart_rate_zone": "75-85%",
+            "total_time": "20 minutes"
+        },
+        # Other short workouts...
+    ],
+    "Medium": [
+        {
+            "title": "Medium Workout 1",
+            "description": "Description for medium workout...",
+            "heart_rate_zone": "70-80%",
+            "total_time": "40 minutes"
+        },
+        # Other medium workouts...
+    ],
+    "Long": [
+        {
+            "title": "Long Workout 1",
+            "description": "Description for long workout...",
+            "heart_rate_zone": "65-75%",
+            "total_time": "60 minutes"
+        },
+        # Other long workouts...
+    ],
+}
+
+@app.route('/')
+def index():
+    return render_template('erg_workouts.html', workout_groups=workout_groups, workout_of_the_day="Sample Workout of the Day")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ generate_static.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+import app
+
+# Define the Jinja2 Environment
+env = Environment(loader=FileSystemLoader("templates"))
+
+# Load the template
+template = env.get_template("erg_workouts.html")
+
+# Sample workout data (same as in the previous example)
+workout_groups = app.workout_groups
+
+# Render the template with the workout data
+static_html = template.render(workout_groups=workout_groups, workout_of_the_day="Sample Workout of the Day")
+
+# Write the rendered template to a static HTML file
+with open("index.html", "w") as f:
+    f.write(static_html)
