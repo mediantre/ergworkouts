9238e8f924932efff5929bc6990d13b2d1b5e89f
John Henry Mejia <jmejia@vtpy10gh1j.office.atlassian.com>
2023-06-07 11:56:28 -0500

    ad workout

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ workout_groups = {
     "Short": [
         {
             "title": "2 x 2000m rate increase",
-            "description": "Row two 2000 meter pieces...",
-            "heart_rate_zone": "75-85%",
+            "description": "2 x 2000m rate increase / 4 min easy  @2k + 8-10 (can go less at higher spm). <br> Row two 2000 meter pieces. In each piece, row the first 1000 meters @ 26 spm. Then 500 meters @ 28 spm, 250 meters @ 30 spm and 250 meters @ 32 spm. Row for four minutes at light pressure during the rest period.",
+            "heart_rate_zone": "UT1",
             "total_time": "20 minutes"
         },
-        # Other short workouts...
+        {
+            "title": "6 x 500m / 2:00 rest",
+            "description": "6 x 500m / 2:00 rest @2k - 1 - 3 . <br> Row six 500 meter pieces. Row or rest for two minutes at light pressure between each 500.",
+        }
     ],
     "Medium": [
         {
