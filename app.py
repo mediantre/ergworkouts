18c86ba09761a02609e6ba224b76257660f0ba6b
John Henry Mejia <jmejia@vtpy10gh1j.office.atlassian.com>
2023-07-24 17:50:09 -0500

    Update template to include c2 wod

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import requests
+import json
+import random
 
 app = Flask(__name__)
 
@@ -380,11 +383,25 @@ workout_groups = {
     ],
 }
 
-workout_of_the_day = "6 x 500m / 2:00 rest @2k-2-3. Goal is to hold the same split for all 6 pieces- DONT FLY AND DIE. Rate: Open"
+# pick a random workout from the list
+
+workout_of_the_day = random.choice(workout_groups[random.choice(list(workout_groups.keys()))])
+
+workout_of_the_day = "{} ○ \nDescription: {} ○ \nHeart Rate Zone: {} ○ \nTotal Time: {}".format(
+    workout_of_the_day['title'],
+    workout_of_the_day['description'],
+    workout_of_the_day['heart_rate_zone'],
+    workout_of_the_day['total_time']
+)
+
+
+# https://api-v4.concept2.com/wod/today
+
+c2_workout_of_the_day = json.loads(requests.get('https://api-v4.concept2.com/wod/today').text)["description"]["en"]
 
 @app.route('/')
 def index():
-    return render_template('erg_workouts.html', workout_groups=workout_groups, workout_of_the_day=workout_of_the_day)
+    return render_template('erg_workouts.html', workout_groups=workout_groups, workout_of_the_day=workout_of_the_day, c2_workout_of_the_day=c2_workout_of_the_day)
 
 if __name__ == '__main__':
     app.run(debug=True)
